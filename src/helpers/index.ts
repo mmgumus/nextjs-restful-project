ca8ad92b5865fa1f1abde088978dceea78416749
Muhammet Mahmut Gümüş <56482835+mmgumus@users.noreply.github.com>
2023-11-13 11:03:43 +0300

    mangodb ve authentication eklendi

---
+++ src/helpers/index.ts
@@ -0,0 +1,4 @@
+import crypto from 'crypto';
+const Secret = 'mysecretkey';
+export const random = () => crypto.randomBytes(128).toString('base64');
+export const authentication = (salt: string, password: string) => {return crypto.createHmac('sha256',[salt,password].join('/')).update(Secret).digest('hex')}; 
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParse from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -21,3 +22,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log('Server is running at http://localhost:8080');
 });
+
+const MONGO_URL = 'mongodb+srv://mahmut:mahmut321@cluster0.r7dml7x.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error',(error: Error) => console.log(error));
